5792867ec68687d38e27f1485389328b7a7918a4
Gregory Sastrawidjaya <greg@gung.se>
2023-03-17 21:39:35 +0100

    button: implement charge incrementing

--- Temperature/Button.gd
+++ Temperature/Button.gd
@@ -1,15 +1,29 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+# Hold down left click within the collision, should then increase static value from 1-100
+const CHARGE_LIMIT: int = 150;
+var isActivated: bool = false;
+var charge: int = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass; # Replace with function body.
 
+func _input(event):
+	pass;
+
+func _physics_process(delta):
+	if (Input.is_mouse_button_pressed(1)):
+		isActivated = true;
+		increase_charge(isActivated);
+	
+func increase_charge(isActivated: bool):
+	if (charge > CHARGE_LIMIT):
+		charge = CHARGE_LIMIT;
+	elif (charge < CHARGE_LIMIT):
+		charge += 1;
+	
+	print(charge)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
