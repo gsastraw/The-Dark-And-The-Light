6d2931eeeb7e526099d0b27b95e88c92c0bef11d
Ed <gusdanieed@student.gu.se>
2023-03-18 17:46:41 +0100

    drag items

---
+++ Items/GrillItem.gd
@@ -0,0 +1,18 @@
+extends Spatial
+export(PackedScene) var grillItem
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	# add_child(grillItem)
+	# grillItem.global_position = Vector3(0,0,0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- Level/Level.gd
+++ Level/Level.gd
@@ -1,15 +1,27 @@
 extends Node2D
+var held_object = null
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	pass # Replace with function body.
+	for node in get_tree().get_nodes_in_group("pickable"):
+		node.connect("clicked", self, "_on_pickable_clicked")
 
+
+func _on_pickable_clicked(object):
+	if !held_object:
+		held_object = object
+		held_object.pickup()
+		
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if held_object and !event.pressed:
+			held_object.drop(Input.get_last_mouse_speed())
+			held_object = null
 
 
 func _process(delta):
 	var texture = $Viewport.get_texture()
-	$Position2D/Screen.texture = texture
+	$Draggable/Position2D/Screen.texture = texture
 	$ScoreUI/LabelNumber.text = str(PlayerVariables.SCORE)
